7ea197e80beda15a96233958feb171fa303a48c7
alizain143 <alizainzain544@gamil.com>
2022-10-08 12:41:20 -0700

    abcd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter,Navigate,Route,Routes, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Main from "./components/Main";
+
 
 
 function App() {
+ 
   return (
-  <></>
+    <BrowserRouter>
+  
+    <Routes>
+    
+     
+   {localStorage.getItem("token")? <Route path="/*" element={<Main/>}></Route>: ""}
+     <Route path="/login" element={<Login/>}></Route>
+     <Route path="/signup" element={<Signup/>}></Route>
+    </Routes>
+   
+    </BrowserRouter>
   )
 }
 
