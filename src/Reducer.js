6ce219941e386b85e4fa44e02b675705ae799667
alizain143 <alizainzain544@gmail.com>
2022-10-11 04:04:10 -0700

    change 3

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,147 +1,137 @@
-import {legacy_createStore} from "redux"
+import { combineReducers, legacy_createStore } from "redux";
 
- export function emailLogin(e){
-    return{
-        type:"EMAIL_LOGIN",
-        payload:e
-    }
- }
- export function emailSignup(e){
-    return{
-        type:"EMAIL_SIGNUP",
-        payload:e
-    }
- }
- export function passwordLogin(e){
-    return{
-        type:"PASS_LOG",
-        payload:e
-    }
- }
- export function passwordSignup(e){
-    return{
-        type:"PASS_SIGN",
-        payload:e
-    }
- }
- export function conPassword(e){
-    return{
-        type:"CON_PASS",
-        payload:e
-    }
- }
-const initDataLogin={
-    loginMail:'',
-    loginPass:'',
-    signMail:'',
-    signPass:'',
-    conPass:''
+export function emailLogin(e) {
+  return {
+    type: "EMAIL_LOGIN",
+    payload: e,
+  };
 }
-function reducerLogin(state=initDataLogin,action){
-    switch(action.type){
-        case "EMAIL_LOGIN":
-            return {
-                ...state,
-                loginMail:action.payload
-            }
-        case "EMAIL_SIGNUP":
-            return {
-                ...state,
-                signMail:action.payload
-            }
-        case "PASS_LOG":
-            return {
-                ...state,
-                loginPass:action.payload
-            }
-        case "PASS_SIGN":
-            return {
-                ...state,
-                signPass:action.payload
-            }
-        case "CON_PASS":
-            return {
-                ...state,
-                conPass:action.payload
-            }
-
-        default:
-            return{
-                state
-            }
-    }
- }
-
- export function tranType(e){
-    return{
-        type:"TRAN_TYPE",
-        payload:e
-    }
- }
- export function tranName(e){
-    return{
-        type:"TRAN_NAME",
-        payload:e
-    }
- }
- export function tranAmount(e){
-    return{
-        type:"TRAN_AMOUNT",
-        payload:e
-    }
- }
- export function tranComments(e){
-    return{
-        type:"TRAN_COMM",
-        payload:e
-    }
+export function emailSignup(e) {
+  return {
+    type: "EMAIL_SIGNUP",
+    payload: e,
+  };
 }
- 
-    const initDataTran={
-      type:"",
-      name:"",
-      amount:"",
-      comments:""
-    }
- 
- function reducerTran(state=initDataTran,action){
-     switch(action.type){
-       case 'TRAN_TYPE' :
-        return{
-            ...state,
-            type:action.payload
-        }
-       case 'TRAN_NAME' :
-        return{
-            ...state,
-            name:action.payload
-        }
-       case 'TRAN_AMOUNT' :
-        return{
-            ...state,
-            amount:action.payload
-        }
-       case 'TRAN_COMM' :
-        return{
-            ...state,
-            comments:action.payload
-        }
-     }
-      
- }
+export function passwordLogin(e) {
+  return {
+    type: "PASS_LOG",
+    payload: e,
+  };
+}
+export function passwordSignup(e) {
+  return {
+    type: "PASS_SIGN",
+    payload: e,
+  };
+}
+export function conPassword(e) {
+  return {
+    type: "CON_PASS",
+    payload: e,
+  };
+}
+const initDataLogin = {
+  loginMail: "",
+  loginPass: "",
+  signMail: "",
+  signPass: "",
+  conPass: "",
+};
+function reducerLogin(state = initDataLogin, action) {
+  switch (action.type) {
+    case "EMAIL_LOGIN":
+      return {
+        ...state,
+        loginMail: action.payload,
+      };
+    case "EMAIL_SIGNUP":
+      return {
+        ...state,
+        signMail: action.payload,
+      };
+    case "PASS_LOG":
+      return {
+        ...state,
+        loginPass: action.payload,
+      };
+    case "PASS_SIGN":
+      return {
+        ...state,
+        signPass: action.payload,
+      };
+    case "CON_PASS":
+      return {
+        ...state,
+        conPass: action.payload,
+      };
 
+    default:
+      return {
+        state,
+      };
+  }
+}
 
+export function tranType(e) {
+  return {
+    type: "TRAN_TYPE",
+    payload: e,
+  };
+}
+export function tranName(e) {
+  return {
+    type: "TRAN_NAME",
+    payload: e,
+  };
+}
+export function tranAmount(e) {
+  return {
+    type: "TRAN_AMOUNT",
+    payload: e,
+  };
+}
+export function tranComments(e) {
+  return {
+    type: "TRAN_COMM",
+    payload: e,
+  };
+}
 
+const initDataTran = {
+  type: "",
+  name: "",
+  amount: "",
+  comments: "",
+};
 
+function reducerTran(state = initDataTran, action) {
+  switch (action.type) {
+    case "TRAN_TYPE":
+      return {
+        ...state,
+        type: action.payload,
+      };
+    case "TRAN_NAME":
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case "TRAN_AMOUNT":
+      return {
+        ...state,
+        amount: action.payload,
+      };
+    case "TRAN_COMM":
+      return {
+        ...state,
+        comments: action.payload,
+      };
+    default:
+      return {
+        state,
+      };
+  }
+}
+const rootReducer = combineReducers({ reducerLogin, reducerTran });
 
-
-
-
-
-
-
-
-
-
-
-export const store = legacy_createStore(reducerLogin)
+export const store = legacy_createStore(rootReducer);
--- src/components/Updates.js
+++ src/components/Updates.js
@@ -1,35 +1,76 @@
 import React from "react";
+import { connect } from "react-redux";
+import { tranAmount, tranComments, tranName, tranType } from "../Reducer";
 
-export default function Updates() {
+function Updates(props) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="updates">
       <h1>Updates</h1>
       <div className="updform-d">
-        <form action="" >
+        <form action="" onSubmit={(e) => handleSubmit(e)}>
           <div>
             <label htmlFor="">Transation Type*</label>
             <br />
-            <select name="select-upd" id="" required>
+            <select
+              name="select-upd"
+              id=""
+              required
+              onChange={(e) =>
+                props.tranType(e.target.selectedOptions[0].innerText)
+              }
+            >
               <option value="">--Select a Transaction Type--</option>
               <option value="">Expense</option>
               <option value="">Income</option>
-            </select><br />
+            </select>
+            <br />
           </div>
           <div>
-            <label htmlFor="">Name*</label><br />
-             <input type="text"  placeholder="With whom did this transaction took place ?"/><br />
+            <label htmlFor="">Name*</label>
+            <br />
+            <input
+              type="text"
+              placeholder="With whom did this transaction took place ?"
+              onChange={(e) => props.tranName(e.target.value)}
+            />
+            <br />
           </div>
-           <div>
-             <label htmlFor="">Amount*</label><br />
-             <input type="number" placeholder="How much did you paid or recieved ?" /><br />
-           </div>
-           <div>
-             <label htmlFor="">Comments <span style={{color:"rgba(0,0,0,0.6)" ,fontSize:"0.9rem"}}>(optional)</span></label><br />
-             <input type="text" placeholder="Any additonal comments regarding the transaction !" />
-           </div>
-           <input type="submit" value="Update"/>
+          <div>
+            <label htmlFor="">Amount*</label>
+            <br />
+            <input
+              type="number"
+              placeholder="How much did you paid or recieved ?"
+              onChange={(e) => props.tranAmount(e.target.value)}
+            />
+            <br />
+          </div>
+          <div>
+            <label htmlFor="">
+              Comments{" "}
+              <span style={{ color: "rgba(0,0,0,0.6)", fontSize: "0.9rem" }}>
+                (optional)
+              </span>
+            </label>
+            <br />
+            <input
+              type="text"
+              placeholder="Any additonal comments regarding the transaction !"
+              onChange={(e) => props.tranComments(e.target.value)}
+            />
+          </div>
+          <input type="submit" value="Update" />
         </form>
       </div>
     </div>
   );
 }
+const mapstatetoProps = (gState) => {
+  return gState;
+};
+const mapDispatchtoProps = { tranAmount, tranComments, tranName, tranType };
+
+export default connect(mapstatetoProps, mapDispatchtoProps)(Updates);
